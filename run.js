749ac07f2fa6b376cb85f056d3abb39869d729a8
R0bertus <hans.gillis.it@gmail.com>
2019-01-26 22:37:54 +0100

    fix #3 cdn.jsdelivr

--- run.js
+++ run.js
@@ -5,7 +5,7 @@ var loop = function(code, interval=0.25) {
         if (d.getElementById(id)){ _loop(code, interval); return; }
         js = d.createElement(s); js.id = id;
         js.onload = function(){console.log("AdventureLanThon has been imported")};
-        js.src = "//raw.githubusercontent.com/R0bertus/AdventureLanThon/master/alt/init.js";
+        js.src = "//cdn.jsdelivr.net/gh/R0bertus/AdventureLanThon@master/alt/init.js";
         fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'alt-engine'))
     if(loading == null) {
@@ -19,12 +19,7 @@ var loop = function(code, interval=0.25) {
 };
 
 var code = `
-# Here you can script in python
-# All functionality is a 1-1 relationship
-# with the official runner functions from  https://github.com/kaansoral/adventureland
-# Unofficial documentation -> https://nexusnull.github.io/adventureland/index.html
-# ***This is a work in progress***
-
 set_message("ALT loaded")
-
 `
+
+loop(code)
