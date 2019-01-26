5f8b8a051d74f1db02fa5bd791869c1bca7ed89f
R0bertus <hans.gillis.it@gmail.com>
2019-01-26 23:55:12 +0100

    debugging cdn #1

--- alt/init.js
+++ alt/init.js
@@ -8,10 +8,9 @@ function _loop(code, interval=0.25) {
                 if(request.readyState === 4) {
                     if(request.status === 200 || request.status == 0) {
                         var ALT_core = request.responseText;
-                        console.log(ALT_core + code)
-                        return;
                         setInterval(function () {
                             console.log(code)
+                            console.log(ALT_core)
                             console.log(
                                 __BRYTHON__.python_to_js(ALT_core + code)
                             )
